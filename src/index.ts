8aaf0bee08fa332e5f1495a163b919b85c5a22b8
Yousef H <76191379+wi@users.noreply.github.com>
2023-05-16 20:57:38 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import { Line, spotify } from "~spotify";
 require('dotenv').config();
 
 const client = new spotify(process.env.CLIENTID, process.env.CLIENTSECRET, process.env.cookie)
+client.emitLyric = true
 client.update()
-client.event.on('lyricUpdate', (newLine: Line, Lines: Array<Line>) => {
+client.on('lyricUpdate', (newLine: Line, Lines: Array<Line>) => {
     console.log(newLine.text)
 })
